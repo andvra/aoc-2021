7bed64fcd20e33109e9978fbdd3ce312e485c44a
Andreas <andreas.vralstad@gmail.com>
2025-01-18 21:19:34 +0100

    Day 3

--- Days.cs
+++ Days.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+
 public class Aoc2021
 {
-    static string path_root = "C:\\Users\\andre\\source\\repos\\aoc-2021\\input";
-
     public struct vec2d
     {
-        public vec2d(Int64 newx, Int64 newy)
+        public vec2d(long newx, long newy)
         {
             x = newx;
             y = newy;
@@ -23,12 +17,12 @@ public class Aoc2021
         {
             return new vec2d(v1.x + v2.x, v1.y + v2.y);
         }
-        public Int64 x;
-        public Int64 y;
+        public long x;
+        public long y;
     }
     public struct vec3d
     {
-        public vec3d(Int64 newx, Int64 newy, Int64 newz)
+        public vec3d(long newx, long newy, long newz)
         {
             x = newx;
             y = newy;
@@ -42,65 +36,19 @@ public class Aoc2021
         {
             return new vec3d(v1.x + v2.x, v1.y + v2.y, v1.z + v2.z);
         }
-        public Int64 x;
-        public Int64 y;
-        public Int64 z;
+        public long x;
+        public long y;
+        public long z;
     }
 
-
-    public static void run()
-    {
-        foreach (var idx_day in Enumerable.Range(1, 25))
-        {
-            foreach (var idx_part in Enumerable.Range(1, 2))
-            {
-                var func = typeof(Aoc2021).GetMethod($"day{idx_day}_part{idx_part}");
-                if (func == null)
-                {
-                    continue;
-                }
-
-                Int64 ret_test = -1;
-                Int64 ret_real = -1;
-
-                foreach (var idx_type in Enumerable.Range(1, 2))
-                {
-                    var is_real = idx_type == 2;
-                    var fn = Path.Join(path_root, $"day{idx_day}_{(is_real ? "real" : "test")}.txt");
-                    if (!File.Exists(fn))
-                    {
-                        continue;
-                    }
-                    var lines = File.ReadAllLines(fn);
-                    if (lines != null && lines.Any())
-                    {
-                        var ret = func.Invoke(null, [lines.ToList(), is_real]);
-                        if (ret != null)
-                        {
-                            var int_val = (Int64)ret;
-                            if (is_real)
-                            {
-                                ret_real = int_val;
-                            }
-                            else
-                            {
-                                ret_test = int_val;
-                            }
-                        }
-                    }
-                }
-                Console.WriteLine($"Day {idx_day} part {idx_part}: {ret_test} / {ret_real}");
-            }
-        }
-    }
-    public static Int64 day1_part1(List<string> lines, bool is_real)
+    public static long day1_part1(List<string> lines, bool is_real)
     {
         return lines
             .Zip(lines.Skip(1))
             .Select(x => int.Parse(x.First) < int.Parse(x.Second))
             .Count(x => x);
     }
-    public static Int64 day1_part2(List<string> lines, bool is_real)
+    public static long day1_part2(List<string> lines, bool is_real)
     {
         var sums = lines
             .Zip(lines.Skip(1), lines.Skip(2))
@@ -111,7 +59,7 @@ public class Aoc2021
             .Select(x => x.First < x.Second)
             .Count(x => x);
     }
-    public static Int64 day2_part1(List<string> lines, bool is_real)
+    public static long day2_part1(List<string> lines, bool is_real)
     {
         var c2v = new Dictionary<char, vec2d>
         {
@@ -126,7 +74,7 @@ public class Aoc2021
         return v.x * v.y;
     }
 
-    public static Int64 day2_part2(List<string> lines, bool is_real)
+    public static long day2_part2(List<string> lines, bool is_real)
     {
         var c2v = new Dictionary<char, vec3d>
         {
@@ -141,7 +89,7 @@ public class Aoc2021
         return v.x * v.y;
     }
 
-    public static Int64 day3_part1(List<string> lines, bool is_real)
+    public static long day3_part1(List<string> lines, bool is_real)
     {
         var bin_arr = lines
             .Select(x => x.ToCharArray().Select(x => x - '0'))
@@ -152,5 +100,45 @@ public class Aoc2021
         var num = Convert.ToInt64(bin_chars, 2);
         var num_inv = (1 << lines[0].Length) - 1 - num;
         return num * num_inv;
+    }
+
+    public static long day3_part2(List<string> lines, bool is_real)
+    {
+        var bin_arr = lines
+            .Select(x => x.ToCharArray().Select(y => y - '0').ToList())
+            .ToList();
+        var all_lines = Enumerable.Range(0, lines.Count()).ToList();
+        var lines_last_level = new List<List<int>> { all_lines, all_lines };
+        var ratings = new List<int> { 0, 0 };
+        foreach (var col in Enumerable.Range(0, bin_arr[0].Count()))
+        {
+            for (var idx_rating = 0; idx_rating < 2; idx_rating++)
+            {
+                var num_ones = lines_last_level[idx_rating]
+                    .Select(x => bin_arr[x][col])
+                    .Sum();
+                var num_zeros = lines_last_level[idx_rating].Count() - num_ones;
+                var to_keep = -1;
+                if (idx_rating == 0)
+                {
+                    to_keep = num_ones >= num_zeros ? 1 : 0;
+                }
+                else
+                {
+                    to_keep = num_zeros <= num_ones ? 0 : 1;
+                }
+                lines_last_level[idx_rating] = lines_last_level[idx_rating]
+                    .Where(x => bin_arr[x][col] == to_keep)
+                    .ToList();
+                if (lines_last_level[idx_rating].Count() == 1)
+                {
+                    var idx_line_win = lines_last_level[idx_rating][0];
+                    var rating = Convert.ToInt32(lines[idx_line_win], 2);
+                    ratings[idx_rating] = rating;
+                }
+            }
+        }
+
+        return ratings[0] * ratings[1];
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,1 +1,44 @@
-﻿Aoc2021.run();
+﻿string path_root = "C:\\Users\\andre\\source\\repos\\aoc-2021\\input";
+
+foreach (var idx_day in Enumerable.Range(1, 25))
+{
+    foreach (var idx_part in Enumerable.Range(1, 2))
+    {
+        var func = typeof(Aoc2021).GetMethod($"day{idx_day}_part{idx_part}");
+        if (func == null)
+        {
+            continue;
+        }
+
+        Int64 ret_test = -1;
+        Int64 ret_real = -1;
+
+        foreach (var idx_type in Enumerable.Range(1, 2))
+        {
+            var is_real = idx_type == 2;
+            var fn = Path.Join(path_root, $"day{idx_day}_{(is_real ? "real" : "test")}.txt");
+            if (!File.Exists(fn))
+            {
+                continue;
+            }
+            var lines = File.ReadAllLines(fn);
+            if (lines != null && lines.Any())
+            {
+                var ret = func.Invoke(null, [lines.ToList(), is_real]);
+                if (ret != null)
+                {
+                    var int_val = (long)ret;
+                    if (is_real)
+                    {
+                        ret_real = int_val;
+                    }
+                    else
+                    {
+                        ret_test = int_val;
+                    }
+                }
+            }
+        }
+        Console.WriteLine($"Day {idx_day} part {idx_part}: {ret_test} / {ret_real}");
+    }
+}
